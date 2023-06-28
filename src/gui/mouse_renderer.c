3e04c718b175a254325ad69c653140eb17b66c92
jnngl <jnngles@gmail.com>
2023-06-29 09:58:09 +1100

    Draw cursor directly to linear framebuffer

--- src/gui/mouse_renderer.c
+++ src/gui/mouse_renderer.c
@@ -3,23 +3,14 @@
 #include "../mouse.h"
 #include "../lfb.h"
 
-static uint32_t screen_buffer[CURSOR_WIDTH * CURSOR_HEIGHT];
 static uint8_t cursor_rendered = 0;
 
 static uint32_t* cursor;
 
-void mouse_fill_buffer() {
-    for (uint32_t y = 0; y < CURSOR_HEIGHT; y++) {
-        for (uint32_t x = 0; x < CURSOR_WIDTH; x++) {
-            screen_buffer[y * CURSOR_WIDTH + x] = lfb_get_pixel(mouse_x + x, mouse_y + y);
-        }
-    }
-}
-
 void mouse_restore_buffer() {
     for (uint32_t y = 0; y < CURSOR_HEIGHT; y++) {
         for (uint32_t x = 0; x < CURSOR_WIDTH; x++) {
-            lfb_set_pixel(last_mouse_x + x, last_mouse_y + y, screen_buffer[y * CURSOR_WIDTH + x]);
+            lfb_set_pixel(last_mouse_x + x, last_mouse_y + y, lfb_get_pixel(last_mouse_x + x, last_mouse_y + y));
         }
     }
 }
@@ -39,8 +30,6 @@ void mouse_render_cursor() {
         cursor_rendered = 1;
     }
 
-    mouse_fill_buffer();
-
     for (uint32_t y = 0; y < CURSOR_HEIGHT; y++) {
         for (uint32_t x = 0; x < CURSOR_WIDTH; x++) {
             uint32_t color = cursor[y * CURSOR_WIDTH + x];
@@ -48,7 +37,7 @@ void mouse_render_cursor() {
                 continue;
             }
 
-            lfb_set_pixel(mouse_x + x, mouse_y + y, color);
+            *((uint32_t*) linear_framebuffer + mouse_x + x + (mouse_y + y) * lfb_width) = color;
         }
     }
 }
